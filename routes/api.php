ca1b3790b609b2ea3326fd9b0cd09f15fa06d631
eric-motta <eric@greenn.com.br>
2025-03-12 10:57:24 -0300

    improvment: queue mvp

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\ProcessUserJob;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('',  function () {
+    $data = ['name' => 'John Doe', 'email' => 'john@example.com'];
+
+    dispatch(new ProcessUserJob($data))->onQueue('user');
     return 'OK';
 });
